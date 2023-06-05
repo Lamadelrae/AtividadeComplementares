e9ecbd178abb4c308bd2c284b6e0c951a48728bb
Lamadelrae <matthew.almeida572@gmail.com>
2023-06-05 14:34:03 -0300

    Add project files.

---
+++ AtividadesComplementaresTests/RegistroTests.cs
@@ -0,0 +1,78 @@
+﻿using AtividadeComplementares.Models;
+using Microsoft.Win32;
+
+namespace AtividadesComplementaresTests
+{
+    [TestClass]
+    public class RegistroTests
+    {
+        [TestMethod]
+        public void Deve_Criar_Registro_ComAtividadeComplementar()
+        {
+            //arrange
+            var aluno = new Aluno("Matthew", "123@gmail.com");
+            var atividadeComplementar = new AtividadeComplementar("Fundamentos SQL", DateTime.Now, 2);
+
+            //act
+            var registro = new Registro(aluno, atividadeComplementar);
+
+            //assert
+            Assert.IsNotNull(registro);
+            Assert.AreEqual(aluno, registro.Aluno); // mesma referência
+            Assert.AreEqual(atividadeComplementar, registro.AtividadeComplementar); // mesma referência
+            Assert.IsTrue(registro.DataValidacao <= DateTime.Now);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Registro_ComPalestra()
+        {
+            //arrange
+            var aluno = new Aluno("Matthew", "123@gmail.com");
+            var palestra = new Palestra("Fundamentos SQL", DateTime.Now, 2, "Matthew");
+
+            //act
+            var registro = new Registro(aluno, palestra);
+
+            //assert
+            Assert.IsNotNull(registro);
+            Assert.AreEqual(aluno, registro.Aluno); // mesma referência
+            Assert.AreEqual(palestra, registro.AtividadeComplementar); // mesma referência
+            Assert.IsTrue(registro.DataValidacao <= DateTime.Now);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Registro_ComMiniCurso()
+        {
+            //arrange
+            var aluno = new Aluno("Matthew", "123@gmail.com");
+            var miniCurso = new MiniCurso("Fundamentos SQL", DateTime.Now, 2, "Matthew");
+
+            //act
+            var registro = new Registro(aluno, miniCurso);
+
+            //assert
+            Assert.IsNotNull(registro);
+            Assert.AreEqual(aluno, registro.Aluno); // mesma referência
+            Assert.AreEqual(miniCurso, registro.AtividadeComplementar); // mesma referência
+            Assert.IsTrue(registro.DataValidacao <= DateTime.Now);
+        }
+
+        [TestMethod]
+        public void Deve_Criar_Registro_E_Jogar_Exception()
+        {
+            //arrange
+            var aluno = new Aluno("Matthew", "123@gmail.com");
+            var atividadeComplementar = new AtividadeComplementar("Fundamentos SQL", DateTime.Now.AddDays(2), 2);
+            var registro = new Registro(aluno, atividadeComplementar);
+
+            //act
+            var action = () => registro.RegistrarAtividadeComplementar();
+
+            //assert
+            Assert.IsNotNull(registro);
+            Assert.AreEqual(aluno, registro.Aluno); // mesma referência
+            Assert.AreEqual(atividadeComplementar, registro.AtividadeComplementar); // mesma referência
+            Assert.ThrowsException<InvalidOperationException>(action, "Data de validação não pode ser anterior a de realização.");
+        }
+    }
+}
